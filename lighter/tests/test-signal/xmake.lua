8fbec7bcdb549e0d79bdee176b3c4edafff14a83
Locietta <locietta@gmail.com>
2026-08-02 10:22:35 +0800

    lighter/tests: add pthread link for test-signal
    it uses std::thread, so pthread is needed on linux

--- lighter/tests/test-signal/xmake.lua
+++ lighter/tests/test-signal/xmake.lua
@@ -3,4 +3,7 @@ target("test-signal")
     set_group("test")
     add_files("main.cpp")
     add_deps("lighter")
+    if is_plat("linux") then
+        add_syslinks("pthread")
+    end
     add_tests("default")
