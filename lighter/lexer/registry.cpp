f7e1bd301192c7ce650a6dcaf788a6db9e451aba
Locietta <locietta@gmail.com>
2026-08-09 02:04:07 +0800

    feat(lexer): register all Markdown language names

--- lighter/lexer/registry.cpp
+++ lighter/lexer/registry.cpp
@@ -1,65 +1,164 @@
 #include "registry.h"
 
 #include <array>
-#include <span>
 
 #include <lighter/lexer/ascii.h>
+#include <lighter/lexer/language/assembly.h>
+#include <lighter/lexer/language/bash.h>
+#include <lighter/lexer/language/brace.h>
+#include <lighter/lexer/language/build_script.h>
 #include <lighter/lexer/language/cpp.h>
+#include <lighter/lexer/language/css.h>
+#include <lighter/lexer/language/document_markup.h>
+#include <lighter/lexer/language/functional.h>
 #include <lighter/lexer/language/go.h>
 #include <lighter/lexer/language/javascript.h>
+#include <lighter/lexer/language/legacy.h>
+#include <lighter/lexer/language/markup.h>
 #include <lighter/lexer/language/python.h>
 #include <lighter/lexer/language/rust.h>
+#include <lighter/lexer/language/scripting.h>
+#include <lighter/lexer/language/sql.h>
+#include <lighter/lexer/language/structured_data.h>
 
 namespace lighter::lexer {
 
 namespace {
 
-using namespace std::literals::string_view_literals;
+using namespace std::literals;
 
 using Factory = Lexer (*)();
 
 struct LanguageBinding {
-    std::span<const std::string_view> names;
+    std::string_view names;
     Factory create;
 };
-
-constexpr std::array k_cpp_names = {
-    "c"sv, "c++"sv, "cc"sv, "cpp"sv, "cxx"sv, "h"sv, "hh"sv, "hpp"sv, "hxx"sv, "idl"sv, "midl"sv, "odl"sv,
-};
-constexpr std::array k_objective_names = {
-    "m"sv, "mm"sv, "obj-c"sv, "obj-c++"sv, "objc"sv, "objc++"sv, "objcpp"sv, "objective-c"sv, "objective-c++"sv, "objective-cpp"sv,
-};
-constexpr std::array k_resource_names = {"dlg"sv, "rc"sv, "rc2"sv, "rct"sv, "resource"sv, "resource-script"sv, "rh"sv};
-constexpr std::array k_rust_names = {"rs"sv, "rust"sv};
-constexpr std::array k_python_names = {"py"sv, "python"sv, "python3"sv};
-constexpr std::array k_javascript_names = {"cjs"sv, "javascript"sv, "js"sv, "jse"sv, "jsm"sv, "mjs"sv, "qs"sv};
-constexpr std::array k_jsx_names = {"jsx"sv};
-constexpr std::array k_typescript_names = {"cts"sv, "ets"sv, "mts"sv, "osts"sv, "ts"sv, "typescript"sv};
-constexpr std::array k_tsx_names = {"tsx"sv};
-constexpr std::array k_go_names = {"go"sv, "golang"sv, "gop"sv, "ql"sv};
 
 template <auto ConfiguredLexer>
 [[nodiscard]] Lexer create_configured_lexer() {
     return make_lexer(ConfiguredLexer);
 }
 
+// Each binding lists canonical Markdown fence names and common ecosystem
+// aliases separated by '|'. The table is deliberately explicit: adding a
+// language is an ordinary constexpr edit with no generated catalogue or
+// registration macro.
 constexpr std::array k_registry = {
-    LanguageBinding{.names = k_cpp_names, .create = create_configured_lexer<CppLexer{.dialect = CppDialect::CPP}>},
-    LanguageBinding{.names = k_objective_names, .create = create_configured_lexer<CppLexer{.dialect = CppDialect::OBJECTIVE_C}>},
-    LanguageBinding{.names = k_resource_names, .create = create_configured_lexer<CppLexer{.dialect = CppDialect::RESOURCE_SCRIPT}>},
-    LanguageBinding{.names = k_rust_names, .create = create_configured_lexer<RustLexer{}>},
-    LanguageBinding{.names = k_python_names, .create = create_configured_lexer<PythonLexer{}>},
-    LanguageBinding{
-        .names = k_javascript_names,
-        .create = create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::JAVASCRIPT}>,
-    },
-    LanguageBinding{.names = k_jsx_names, .create = create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::JSX}>},
-    LanguageBinding{
-        .names = k_typescript_names,
-        .create = create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::TYPESCRIPT}>,
-    },
-    LanguageBinding{.names = k_tsx_names, .create = create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::TSX}>},
-    LanguageBinding{.names = k_go_names, .create = create_configured_lexer<GoLexer{}>},
+    LanguageBinding{"c|c++|cc|cpp|cxx|h|hh|hpp|hxx|idl|midl|odl"sv, create_configured_lexer<CppLexer{.dialect = CppDialect::CPP}>},
+    LanguageBinding{"m|mm|obj-c|obj-c++|objc|objc++|objcpp|objective-c|objective-c++|objective-cpp"sv,
+                    create_configured_lexer<CppLexer{.dialect = CppDialect::OBJECTIVE_C}>},
+    LanguageBinding{"dlg|rc|rc2|rct|resource|resource-script|rh"sv,
+                    create_configured_lexer<CppLexer{.dialect = CppDialect::RESOURCE_SCRIPT}>},
+    LanguageBinding{"rs|rust"sv, create_configured_lexer<RustLexer{}>},
+    LanguageBinding{"py|py3|pycon|python|python3"sv, create_configured_lexer<PythonLexer{}>},
+    LanguageBinding{"cjs|javascript|js|jse|jsm|mjs|qs"sv,
+                    create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::JAVASCRIPT}>},
+    LanguageBinding{"actionscript|as|as3"sv, create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::ACTIONSCRIPT}>},
+    LanguageBinding{"jsx"sv, create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::JSX}>},
+    LanguageBinding{"cts|ets|mts|osts|ts|typescript"sv, create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::TYPESCRIPT}>},
+    LanguageBinding{"tsx"sv, create_configured_lexer<JavaScriptLexer{.dialect = JavaScriptDialect::TSX}>},
+    LanguageBinding{"go|golang|gop|ql"sv, create_configured_lexer<GoLexer{}>},
+
+    LanguageBinding{"bash|dash|ksh|sh|shell|shell-script|shellsession|zsh"sv,
+                    create_configured_lexer<BashLexer{.dialect = ShellDialect::BASH}>},
+    LanguageBinding{"csh|tcsh"sv, create_configured_lexer<BashLexer{.dialect = ShellDialect::C_SHELL}>},
+    LanguageBinding{"autoconf|m4"sv, create_configured_lexer<BashLexer{.dialect = ShellDialect::M4}>},
+
+    LanguageBinding{"geojson|ipynb|json|json5|jsonc|jsonl|ndjson|webmanifest"sv,
+                    create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::JSON}>},
+    LanguageBinding{"toml"sv, create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::TOML}>},
+    LanguageBinding{"yaml|yml"sv, create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::YAML}>},
+    LanguageBinding{"cfg|conf|config|dotenv|editorconfig|env|properties"sv,
+                    create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::CONFIG}>},
+    LanguageBinding{"desktop|ini"sv, create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::INI}>},
+    LanguageBinding{"csv"sv, create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::CSV}>},
+    LanguageBinding{"tsv"sv, create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::TSV}>},
+    LanguageBinding{"diff|patch|udiff"sv, create_configured_lexer<StructuredDataLexer{.dialect = StructuredDataDialect::DIFF}>},
+
+    LanguageBinding{"htm|html|shtml|vue|xhtml"sv, create_configured_lexer<MarkupLexer{.dialect = MarkupDialect::HTML}>},
+    LanguageBinding{"csproj|plist|rss|svg|xaml|xml|xsd|xsl|xslt"sv, create_configured_lexer<MarkupLexer{.dialect = MarkupDialect::XML}>},
+    LanguageBinding{"markdown|md|mdown|mkd|mkdn"sv, create_configured_lexer<MarkupLexer{.dialect = MarkupDialect::MARKDOWN}>},
+    LanguageBinding{"css|less|sass|scss"sv, create_configured_lexer<CssLexer{}>},
+
+    LanguageBinding{"java"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::JAVA}>},
+    LanguageBinding{"c#|cs|csharp|dotnet"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::CSHARP}>},
+    LanguageBinding{"d|dlang"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::D}>},
+    LanguageBinding{"dart"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::DART}>},
+    LanguageBinding{"cangjie|cj"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::CANGJIE}>},
+    LanguageBinding{"groovy"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::GROOVY}>},
+    LanguageBinding{"gradle"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::GRADLE}>},
+    LanguageBinding{"haxe|hx"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::HAXE}>},
+    LanguageBinding{"kotlin|kt|kts"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::KOTLIN}>},
+    LanguageBinding{"scala|sc"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::SCALA}>},
+    LanguageBinding{"swift"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::SWIFT}>},
+    LanguageBinding{"zig"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::ZIG}>},
+    LanguageBinding{"asymptote|asy"sv, create_configured_lexer<BraceLexer{.dialect = BraceDialect::ASYMPTOTE}>},
+
+    LanguageBinding{"mysql|pgsql|plsql|postgres|postgresql|sqlite|sql|tsql"sv, create_configured_lexer<SqlLexer{}>},
+    LanguageBinding{"bat|batch|cmd|dosbatch"sv, create_configured_lexer<BuildScriptLexer{.dialect = BuildScriptDialect::BATCH}>},
+    LanguageBinding{"cmake|cmake.in"sv, create_configured_lexer<BuildScriptLexer{.dialect = BuildScriptDialect::CMAKE}>},
+    LanguageBinding{"gn|gni"sv, create_configured_lexer<BuildScriptLexer{.dialect = BuildScriptDialect::GN}>},
+    LanguageBinding{"make|makefile|mk"sv, create_configured_lexer<BuildScriptLexer{.dialect = BuildScriptDialect::MAKE}>},
+    LanguageBinding{"bjam|boost-jam|jam|jamfile"sv, create_configured_lexer<BuildScriptLexer{.dialect = BuildScriptDialect::JAM}>},
+    LanguageBinding{"inno|iss"sv, create_configured_lexer<BuildScriptLexer{.dialect = BuildScriptDialect::INNO_SETUP}>},
+    LanguageBinding{"nsi|nsis|nsh"sv, create_configured_lexer<BuildScriptLexer{.dialect = BuildScriptDialect::NSIS}>},
+
+    LanguageBinding{"aarch64asm|asm|assembly|gas|masm|nasm|x86asm"sv,
+                    create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::ASSEMBLY}>},
+    LanguageBinding{"cil|il|msil"sv, create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::CIL}>},
+    LanguageBinding{"llvm|llvm-ir"sv, create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::LLVM_IR}>},
+    LanguageBinding{"smali"sv, create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::SMALI}>},
+    LanguageBinding{"wasm|wat|webassembly"sv, create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::WEBASSEMBLY}>},
+    LanguageBinding{"sv|svh|systemverilog|v|verilog"sv, create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::VERILOG}>},
+    LanguageBinding{"vhd|vhdl"sv, create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::VHDL}>},
+    LanguageBinding{"whs|winhex"sv, create_configured_lexer<AssemblyLexer{.dialect = AssemblyDialect::WINHEX}>},
+
+    LanguageBinding{"cl|clojure|common-lisp|elisp|emacs-lisp|lisp|racket|scheme"sv,
+                    create_configured_lexer<FunctionalLexer{.dialect = FunctionalDialect::LISP}>},
+    LanguageBinding{"haskell|hs|lhs"sv, create_configured_lexer<FunctionalLexer{.dialect = FunctionalDialect::HASKELL}>},
+    LanguageBinding{"ml|mli|ocaml"sv, create_configured_lexer<FunctionalLexer{.dialect = FunctionalDialect::OCAML}>},
+    LanguageBinding{"f#|fs|fsharp|fsi|fsx"sv, create_configured_lexer<FunctionalLexer{.dialect = FunctionalDialect::FSHARP}>},
+    LanguageBinding{"erl|erlang|hrl"sv, create_configured_lexer<FunctionalLexer{.dialect = FunctionalDialect::ERLANG}>},
+    LanguageBinding{"eex|elixir|ex|exs|heex"sv, create_configured_lexer<FunctionalLexer{.dialect = FunctionalDialect::ELIXIR}>},
+
+    LanguageBinding{"ahk|autohotkey"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::AUTOHOTKEY}>},
+    LanguageBinding{"au3|autoit"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::AUTOIT}>},
+    LanguageBinding{"avisynth|avs"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::AVISYNTH}>},
+    LanguageBinding{"awk|gawk"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::AWK}>},
+    LanguageBinding{"coffee|coffeescript"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::COFFEESCRIPT}>},
+    LanguageBinding{"jl|julia"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::JULIA}>},
+    LanguageBinding{"lua"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::LUA}>},
+    LanguageBinding{"mathematica|mma|nb|wolfram|wolfram-language"sv,
+                    create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::MATHEMATICA}>},
+    LanguageBinding{"matlab"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::MATLAB}>},
+    LanguageBinding{"nim|nimrod"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::NIM}>},
+    LanguageBinding{"perl|pl|pm"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::PERL}>},
+    LanguageBinding{"hack|php|php3|php4|php5|phtml"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::PHP}>},
+    LanguageBinding{"powershell|ps1|psd1|psm1|pwsh"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::POWERSHELL}>},
+    LanguageBinding{"r|rstats"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::R}>},
+    LanguageBinding{"rebol|r3"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::REBOL}>},
+    LanguageBinding{"erb|gemspec|rake|rb|ruby"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::RUBY}>},
+    LanguageBinding{"tcl|tk"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::TCL}>},
+    LanguageBinding{"vim|viml|vimscript"sv, create_configured_lexer<ScriptingLexer{.dialect = ScriptingDialect::VIM}>},
+
+    LanguageBinding{"ansys|apdl"sv, create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::APDL}>},
+    LanguageBinding{"abaqus|inp"sv, create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::ABAQUS}>},
+    LanguageBinding{"f|f03|f08|f77|f90|f95|for|fortran"sv, create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::FORTRAN}>},
+    LanguageBinding{"delphi|freepascal|objectpascal|pas|pascal"sv, create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::PASCAL}>},
+    LanguageBinding{"pbl|powerbuilder|powerscript"sv, create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::POWERBUILDER}>},
+    LanguageBinding{"sas"sv, create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::SAS}>},
+    LanguageBinding{"vb|vb.net|vbnet|visual-basic|visualbasic"sv,
+                    create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::VISUAL_BASIC}>},
+    LanguageBinding{"vbs|vbscript"sv, create_configured_lexer<LegacyLexer{.dialect = LegacyDialect::VBSCRIPT}>},
+
+    LanguageBinding{"dot|dotlang|graphviz|gv"sv, create_configured_lexer<DocumentMarkupLexer{.dialect = DocumentMarkupDialect::GRAPHVIZ}>},
+    LanguageBinding{"actdiag|blockdiag|nwdiag|packetdiag|rackdiag|seqdiag"sv,
+                    create_configured_lexer<DocumentMarkupLexer{.dialect = DocumentMarkupDialect::BLOCKDIAG}>},
+    LanguageBinding{"latex|tex"sv, create_configured_lexer<DocumentMarkupLexer{.dialect = DocumentMarkupDialect::LATEX}>},
+    LanguageBinding{"rest|restructuredtext|rst"sv,
+                    create_configured_lexer<DocumentMarkupLexer{.dialect = DocumentMarkupDialect::RESTRUCTURED_TEXT}>},
+    LanguageBinding{"texi|texinfo"sv, create_configured_lexer<DocumentMarkupLexer{.dialect = DocumentMarkupDialect::TEXINFO}>},
+    LanguageBinding{"typ|typst"sv, create_configured_lexer<DocumentMarkupLexer{.dialect = DocumentMarkupDialect::TYPST}>},
 };
 
 [[nodiscard]] bool equal_ignoring_ascii_case(std::string_view left, std::string_view right) noexcept {
@@ -70,13 +169,22 @@ constexpr std::array k_registry = {
     return true;
 }
 
+[[nodiscard]] bool contains_name(std::string_view names, std::string_view requested) noexcept {
+    while (!names.empty()) {
+        const usize separator = names.find('|');
+        const std::string_view candidate = names.substr(0, separator);
+        if (equal_ignoring_ascii_case(candidate, requested)) return true;
+        if (separator == std::string_view::npos) break;
+        names.remove_prefix(separator + 1);
+    }
+    return false;
+}
+
 } // namespace
 
 std::optional<Lexer> lexer_for_language(std::string_view name) {
     for (const LanguageBinding &binding : k_registry) {
-        for (std::string_view candidate : binding.names) {
-            if (equal_ignoring_ascii_case(name, candidate)) return binding.create();
-        }
+        if (contains_name(binding.names, name)) return binding.create();
     }
     return std::nullopt;
 }
--- lighter/tests/test-lexer-registry/main.cpp
+++ lighter/tests/test-lexer-registry/main.cpp
@@ -1,3 +1,8 @@
+#include <array>
+#include <iostream>
+#include <span>
+#include <string_view>
+
 #include <lighter/lexer/document.h>
 #include <lighter/lexer/registry.h>
 
@@ -5,28 +10,151 @@ namespace {
 
 using namespace lighter;
 using namespace lighter::lexer;
+using namespace std::literals;
 
-bool resolves_aliases() {
-    auto c = lexer_for_language("c");
-    auto cpp = lexer_for_language("C++");
-    auto objective_c = lexer_for_language("objc");
-    auto objective_cpp = lexer_for_language("Objective-C++");
-    auto resource = lexer_for_language("rc");
-    auto idl = lexer_for_language("odl");
-    auto rust = lexer_for_language("rs");
-    auto python = lexer_for_language("Python3");
-    auto javascript = lexer_for_language("mjs");
-    auto jsx = lexer_for_language("JSX");
-    auto typescript = lexer_for_language("TypeScript");
-    auto tsx = lexer_for_language("tsx");
-    auto go = lexer_for_language("golang");
-    return c && (*c)->language_info().id == "cpp" && cpp && (*cpp)->language_info().id == "cpp" && objective_c &&
-           (*objective_c)->language_info().id == "objc" && objective_cpp && (*objective_cpp)->language_info().id == "objc" && resource &&
-           (*resource)->language_info().id == "resource-script" && idl && (*idl)->language_info().id == "cpp" && rust &&
-           (*rust)->language_info().id == "rust" && python && (*python)->language_info().id == "python" && javascript &&
-           (*javascript)->language_info().id == "javascript" && jsx && (*jsx)->language_info().id == "jsx" && typescript &&
-           (*typescript)->language_info().id == "typescript" && tsx && (*tsx)->language_info().id == "tsx" && go &&
-           (*go)->language_info().id == "go";
+struct Resolution {
+    std::string_view name;
+    std::string_view id;
+};
+
+[[nodiscard]] bool resolves(std::span<const Resolution> cases) {
+    for (const Resolution &item : cases) {
+        const auto lexer = lexer_for_language(item.name);
+        if (!lexer || (*lexer)->language_info().id != item.id) {
+            std::cerr << "failed to resolve " << item.name << " as " << item.id << '\n';
+            return false;
+        }
+    }
+    return true;
+}
+
+bool resolves_every_language_identity() {
+    constexpr std::array cases{
+        Resolution{"cpp"sv, "cpp"sv},
+        Resolution{"objc"sv, "objc"sv},
+        Resolution{"resource-script"sv, "resource-script"sv},
+        Resolution{"rust"sv, "rust"sv},
+        Resolution{"python"sv, "python"sv},
+        Resolution{"javascript"sv, "javascript"sv},
+        Resolution{"actionscript"sv, "actionscript"sv},
+        Resolution{"jsx"sv, "jsx"sv},
+        Resolution{"typescript"sv, "typescript"sv},
+        Resolution{"tsx"sv, "tsx"sv},
+        Resolution{"go"sv, "go"sv},
+        Resolution{"bash"sv, "bash"sv},
+        Resolution{"csh"sv, "csh"sv},
+        Resolution{"m4"sv, "m4"sv},
+        Resolution{"json"sv, "json"sv},
+        Resolution{"toml"sv, "toml"sv},
+        Resolution{"yaml"sv, "yaml"sv},
+        Resolution{"config"sv, "config"sv},
+        Resolution{"ini"sv, "ini"sv},
+        Resolution{"csv"sv, "csv"sv},
+        Resolution{"tsv"sv, "tsv"sv},
+        Resolution{"diff"sv, "diff"sv},
+        Resolution{"html"sv, "html"sv},
+        Resolution{"xml"sv, "xml"sv},
+        Resolution{"markdown"sv, "markdown"sv},
+        Resolution{"css"sv, "css"sv},
+        Resolution{"java"sv, "java"sv},
+        Resolution{"csharp"sv, "csharp"sv},
+        Resolution{"dlang"sv, "d"sv},
+        Resolution{"dart"sv, "dart"sv},
+        Resolution{"cangjie"sv, "cangjie"sv},
+        Resolution{"groovy"sv, "groovy"sv},
+        Resolution{"gradle"sv, "gradle"sv},
+        Resolution{"haxe"sv, "haxe"sv},
+        Resolution{"kotlin"sv, "kotlin"sv},
+        Resolution{"scala"sv, "scala"sv},
+        Resolution{"swift"sv, "swift"sv},
+        Resolution{"zig"sv, "zig"sv},
+        Resolution{"asymptote"sv, "asymptote"sv},
+        Resolution{"sql"sv, "sql"sv},
+        Resolution{"batch"sv, "batch"sv},
+        Resolution{"cmake"sv, "cmake"sv},
+        Resolution{"gn"sv, "gn"sv},
+        Resolution{"make"sv, "make"sv},
+        Resolution{"jam"sv, "jam"sv},
+        Resolution{"inno"sv, "inno"sv},
+        Resolution{"nsis"sv, "nsis"sv},
+        Resolution{"asm"sv, "asm"sv},
+        Resolution{"cil"sv, "cil"sv},
+        Resolution{"llvm"sv, "llvm"sv},
+        Resolution{"smali"sv, "smali"sv},
+        Resolution{"wasm"sv, "wasm"sv},
+        Resolution{"verilog"sv, "verilog"sv},
+        Resolution{"vhdl"sv, "vhdl"sv},
+        Resolution{"winhex"sv, "winhex"sv},
+        Resolution{"lisp"sv, "lisp"sv},
+        Resolution{"haskell"sv, "haskell"sv},
+        Resolution{"ocaml"sv, "ocaml"sv},
+        Resolution{"fsharp"sv, "fsharp"sv},
+        Resolution{"erlang"sv, "erlang"sv},
+        Resolution{"elixir"sv, "elixir"sv},
+        Resolution{"autohotkey"sv, "autohotkey"sv},
+        Resolution{"autoit"sv, "autoit"sv},
+        Resolution{"avisynth"sv, "avisynth"sv},
+        Resolution{"awk"sv, "awk"sv},
+        Resolution{"coffeescript"sv, "coffeescript"sv},
+        Resolution{"julia"sv, "julia"sv},
+        Resolution{"lua"sv, "lua"sv},
+        Resolution{"mathematica"sv, "mathematica"sv},
+        Resolution{"matlab"sv, "matlab"sv},
+        Resolution{"nim"sv, "nim"sv},
+        Resolution{"perl"sv, "perl"sv},
+        Resolution{"php"sv, "php"sv},
+        Resolution{"powershell"sv, "powershell"sv},
+        Resolution{"rstats"sv, "r"sv},
+        Resolution{"rebol"sv, "rebol"sv},
+        Resolution{"ruby"sv, "ruby"sv},
+        Resolution{"tcl"sv, "tcl"sv},
+        Resolution{"vim"sv, "vim"sv},
+        Resolution{"apdl"sv, "apdl"sv},
+        Resolution{"abaqus"sv, "abaqus"sv},
+        Resolution{"fortran"sv, "fortran"sv},
+        Resolution{"pascal"sv, "pascal"sv},
+        Resolution{"powerbuilder"sv, "powerbuilder"sv},
+        Resolution{"sas"sv, "sas"sv},
+        Resolution{"vb"sv, "vb"sv},
+        Resolution{"vbscript"sv, "vbscript"sv},
+        Resolution{"graphviz"sv, "graphviz"sv},
+        Resolution{"blockdiag"sv, "blockdiag"sv},
+        Resolution{"latex"sv, "latex"sv},
+        Resolution{"rst"sv, "rst"sv},
+        Resolution{"texinfo"sv, "texinfo"sv},
+        Resolution{"typst"sv, "typst"sv},
+    };
+    return resolves(cases);
+}
+
+bool resolves_common_markdown_aliases() {
+    constexpr std::array cases{
+        Resolution{"C++"sv, "cpp"sv},
+        Resolution{"Objective-C++"sv, "objc"sv},
+        Resolution{"Python3"sv, "python"sv},
+        Resolution{"as"sv, "actionscript"sv},
+        Resolution{"TypeScript"sv, "typescript"sv},
+        Resolution{"shellsession"sv, "bash"sv},
+        Resolution{"jsonc"sv, "json"sv},
+        Resolution{"yml"sv, "yaml"sv},
+        Resolution{"scss"sv, "css"sv},
+        Resolution{"c#"sv, "csharp"sv},
+        Resolution{"kt"sv, "kotlin"sv},
+        Resolution{"postgresql"sv, "sql"sv},
+        Resolution{"makefile"sv, "make"sv},
+        Resolution{"nasm"sv, "asm"sv},
+        Resolution{"wat"sv, "wasm"sv},
+        Resolution{"common-lisp"sv, "lisp"sv},
+        Resolution{"f#"sv, "fsharp"sv},
+        Resolution{"heex"sv, "elixir"sv},
+        Resolution{"ps1"sv, "powershell"sv},
+        Resolution{"delphi"sv, "pascal"sv},
+        Resolution{"vb.net"sv, "vb"sv},
+        Resolution{"dot"sv, "graphviz"sv},
+        Resolution{"restructuredtext"sv, "rst"sv},
+        Resolution{"tex"sv, "latex"sv},
+    };
+    return resolves(cases);
 }
 
 bool dispatches_selected_lexer() {
@@ -43,5 +171,8 @@ bool dispatches_selected_lexer() {
 } // namespace
 
 int main() {
-    return resolves_aliases() && dispatches_selected_lexer() && !lexer_for_language("") && !lexer_for_language("unknown") ? 0 : 1;
+    return resolves_every_language_identity() && resolves_common_markdown_aliases() && dispatches_selected_lexer() &&
+                   !lexer_for_language("") && !lexer_for_language("unknown") ?
+               0 :
+               1;
 }
